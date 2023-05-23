39a6bd65d63646f4cd908ee46f5bbb2a80d2dc8b
Koichi Tanaka <51@tanaka51.jp>
2023-05-23 09:10:04 +0900

    Add test

--- test/integration/not_found_test.rb
+++ test/integration/not_found_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class NotFoundTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'not found return JSON when given "content-type: application/json"' do
+    get '/foo', headers: { 'content-type': 'application/json' }
+
+    assert_response :not_found
+    json = JSON.parse(response.body)
+    assert_equal json, { 'message': 'not found' }
+  end
 end
